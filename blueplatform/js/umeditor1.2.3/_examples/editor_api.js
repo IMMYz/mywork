d6b75e82f39ad507b664855ded9b7101b47fe9a2
叶子 <yz33105@gmail.com>
2017-09-05 00:18:41 +0800

    富文本编辑器完成

--- blueplatform/js/umeditor1.2.3/_examples/editor_api.js
+++ blueplatform/js/umeditor1.2.3/_examples/editor_api.js
@@ -67,7 +67,7 @@
             'adapter/source.js',
             'adapter/combobox.js'
         ],
-        baseURL = '../_src/';
+        baseURL = './js/umeditor1.2.3/_src/';
     for (var i=0,pi;pi = paths[i++];) {
         document.write('<script type="text/javascript" src="'+ baseURL + pi +'"></script>');
     }
